ccf6b2c0d3ced232bcb2e7cb6917add3d7521da5
Detanup01 <91248446+Detanup01@users.noreply.github.com>
2023-06-20 21:45:26 +0200

    pre work on cloudsave

--- Libs/ServerCore/HTTP/CloudSave.cs
+++ Libs/ServerCore/HTTP/CloudSave.cs
@@ -13,18 +13,55 @@ namespace ServerCore.HTTP
         public static string GET(string URL, Dictionary<string, string> headers, out string contentType)
         {
             contentType = "application/json; charset=UTF-8";
+            var userId = headers["UserId"];
+            if (URL.StartsWith("/"))
+                URL = URL.Replace("/cloudsave/", "");
+            else
+                URL = URL.Replace("cloudsave/", "");
+
+            var urlsplit = URL.Split("/");
+            var uplayid = urlsplit[0];
+            var itemOrAll = urlsplit[1];
+            if (itemOrAll.Contains("all"))
+            {
+                //TODO: Reply with all Items
+            }
+            else
+            { 
+                //Todo: ItemId
+            }
             return "{\"ok\":true}";
         }
 
         public static string PUT(string URL, Dictionary<string, string> headers, byte[] body, out string contentType)
         {
             contentType = "text/plain; charset=utf-8";
+            var userId = headers["UserId"];
+            if (URL.StartsWith("/"))
+                URL = URL.Replace("/cloudsave/", "");
+            else
+                URL = URL.Replace("cloudsave/", "");
+
+            var urlsplit = URL.Split("/");
+            var uplayid = urlsplit[0];
+            var itemOrAll = urlsplit[1];
+            //Todo: ItemId
             return "OK!";
         }
 
         public static bool DELETE(string URL, Dictionary<string, string> headers, out string errorRSP)
         {
             errorRSP = "application/json; charset=UTF-8";
+            var userId = headers["UserId"];
+            if (URL.StartsWith("/"))
+                URL = URL.Replace("/cloudsave/", "");
+            else
+                URL = URL.Replace("cloudsave/", "");
+
+            var urlsplit = URL.Split("/");
+            var uplayid = urlsplit[0];
+            var itemOrAll = urlsplit[1];
+            //Todo: ItemId
             return false;
         }
     }
