f377da71580e93691ac56dd08ff2a4c1d0509f1b
Detanup01 <91248446+Detanup01@users.noreply.github.com>
2025-03-26 09:51:42 +0100

    adding OnlineConfigService as plugin

---
+++ Plugins/OnlineConfigService/OnlineService_SVC.cs
@@ -0,0 +1,30 @@
+﻿using ModdableWebServer.Attributes;
+using ModdableWebServer;
+using NetCoreServer;
+using ModdableWebServer.Helper;
+using Serilog;
+
+namespace OnlineConfigService;
+
+public class OnlineService_SVC
+{
+    [HTTP("GET", "/OnlineConfigService.svc/GetOnlineConfig?onlineConfigID={configid}&target={target}")]
+    public static bool GetOnlineConfig(HttpRequest request, ServerStruct serverStruct)
+    {
+        string configid = serverStruct.Parameters["configid"];
+        string target = serverStruct.Parameters["target"];
+
+        var path = Path.Combine("OnlineConfigService", $"{configid}_{target}.json");
+        if (!File.Exists(path))
+        {
+            Log.Warning("OnlineConfigService: Config file not found! ({Path})", path);
+            serverStruct.Response.MakeErrorResponse("No file found!");
+            serverStruct.SendResponse();
+            return true;
+        }
+
+        serverStruct.Response.MakeGetResponse(File.ReadAllText(path), "application/json");
+        serverStruct.SendResponse();
+        return true;
+    }
+}
---
+++ Plugins/OnlineConfigService/Plugin.cs
@@ -0,0 +1,41 @@
+﻿using OnlineConfigService;
+using ServerCore.Controllers;
+using ServerCore.DMX;
+using ServerCore.Extra.Interfaces;
+using System.Composition;
+
+namespace Plugin;
+
+[Export(typeof(IPlugin))]
+public class Plugin : IPlugin, IDisposable
+{
+    public uint Priority => uint.MaxValue;
+
+    public string Name => "OnlineConfigService";
+
+    public Plugin()
+    {
+        Console.WriteLine($"Welcome from {Name} !");
+    }
+
+    public void Dispose()
+    {
+
+    }
+
+    public void Initialize()
+    {
+        ServerController.AddRoutes(typeof(OnlineService_SVC).Assembly);
+    }
+
+
+    public void ShutDown()
+    {
+        ServerController.RemoveRoutes(typeof(OnlineService_SVC).Assembly);
+    }
+
+    public bool DemuxDataReceivedCustom(DmxSession dmxSession, byte[] receivedData, string Protoname)
+    {
+        return false;
+    }
+}
